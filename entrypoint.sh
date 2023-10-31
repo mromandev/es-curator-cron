39259da82cf84bcfcb12de8354118f503465f39c
Marcelo Roman <54809754+mromandev@users.noreply.github.com>
2023-10-31 10:41:37 -0300

    Fix delete-indices command name
    Fix command name
    Adding new customizable values for curator_cli

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ mkdir -p /etc/periodic/${PERIOD}
 cat > /etc/periodic/${PERIOD}/0-curator <<EOF
 #!/bin/sh
 /usr/local/bin/curator_cli $@ \
-    delete_indices \
+    delete-indices \
     --ignore_empty_list \
-    --filter_list '[{"filtertype":"age","source":"name","direction":"older","unit":"days","unit_count":${KEEP_DAYS},"timestring": "%Y.%m.%d"},{"filtertype":"pattern","kind":"prefix","value":"${INDEX_PREFIX}"}]'
+    --filter_list '[{"filtertype":"age","source":"name","direction":"older","unit":"days","unit_count":${KEEP_DAYS},"timestring": "${INDEX_PATTERN}"},{"filtertype":"pattern","kind":"prefix","value":"${INDEX_PREFIX}"}]'
 EOF
 chmod +x /etc/periodic/${PERIOD}/0-curator
 echo "cron: /etc/periodic/${PERIOD}/0-curator"
